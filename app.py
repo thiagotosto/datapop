85ae799598a567d9c3665cf5aa70ba3eb1123c3d
Thiago Tosto <thiagotosto@gmail.com>
2019-05-22 00:56:59 -0300

    Carousel e dumps

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import datetime
 from pymongo import MongoClient
 import os
 import requests as rq
+import json
+import ast
 
 
 
@@ -19,9 +21,9 @@ app.secret_key = 'likujnyhtbgrvf67543'
 @app.route('/get_tweets_sample')
 def getTweetsSample():
     mg_client = MongoClient('localhost', 27017)
-    tweets = mg_cliente.datapop.tweets
+    tweets = mg_client.datapop.tweets_to_classify
 
-    return {obj['_id']: obj for obj in tweets.aggregate([{'$sample': {'size': 10}}])}
+    return json.dumps({str(obj['_id']): {i:obj[i] for i in obj if i != '_id'} for obj in tweets.aggregate([{'$sample': {'size': 3}}])})
 
 @app.route('/classify_tweet/<id>')
 def classifyTweet(id):
@@ -35,10 +37,11 @@ def classifyTweet(id):
 
 @app.route('/')
 def index():
-    #tweets = rq.get('localhost:5000/get_tweets_sample').json()
+    tweets_str = rq.get('http://localhost:5000/get_tweets_sample').text
+    tweets =  json.loads(tweets_str)
 
-    #return tweets
-    return "Hello"
+    return render_template('index.html', tweets=tweets)
+    #return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True)
